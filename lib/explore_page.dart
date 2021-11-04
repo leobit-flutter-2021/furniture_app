9cf4069af2d42bcbfa26bb573b071c2847a241ec
unknown <romanoprisk557@gmail.com>
2021-11-04 15:49:40 +0200

    Skeleton was added

---
+++ lib/explore_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'item_page.dart';
+
+class ExplorePage extends StatefulWidget {
+  const ExplorePage({Key? key}) : super(key: key);
+
+  @override
+  State<ExplorePage> createState() => _ExplorePageState();
+}
+
+class _ExplorePageState extends State<ExplorePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text("Explore"),
+    Text("My Bag"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: 'My Bag',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/my_bag_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyBag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("My Bag"));
+  }
+}
